3f2f1240e2d9e9b499434ad88fdd0c422f4f3d72
Venkat Pendli <venkat.eee718@gmail.com>
2026-07-13 01:06:26 -0400

    config: update backend bucket name for my account

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-YOUR-GITHUB-USERNAME"  # Replace with your S3 bucket name
+    bucket       = "zen-pharma-terraform-state-vegapendli"  # Replace with your S3 bucket name
     key          = "envs/prod/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
--- envs/qa/backend.tf
+++ envs/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-YOUR-GITHUB-USERNAME"  # Replace with your S3 bucket name
+    bucket       = "zen-pharma-terraform-state-vegapendli"  # Replace with your S3 bucket name
     key          = "envs/qa/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
